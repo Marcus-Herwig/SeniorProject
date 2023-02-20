306bf456fd9bafff8158f798654214e8aad57bc3
Marcus-Herwig <70297887+Marcus-Herwig@users.noreply.github.com>
2023-02-20 11:08:05 -0600

    Update CreateTeamChat.xaml.cs

--- SeniorProject/CreateTeamChat.xaml.cs
+++ SeniorProject/CreateTeamChat.xaml.cs
@@ -28,9 +28,9 @@ namespace SeniorProject
         public CreateTeamChat()
         {
             InitializeComponent();
-            this.AzureStorageConnectionString = "https://seniorproject.table.core.windows.net/";
-            this.AzureStorageKey = "cy2AiZ+cJ9/Ft6uXeq7bFVgT2zcSKniQcOGXC955XTbjqvhg1xdN4S34f0ZH9tVEIc3doK4kbxld+AStm9DbtQ==";
-            this.StorageAccountName = "seniorproject";
+            this.AzureStorageConnectionString = "xx";
+            this.AzureStorageKey = "xx";
+            this.StorageAccountName = "xx";
             this.client = new TableClient(new Uri(this.AzureStorageConnectionString), "Accounts", new TableSharedKeyCredential(this.StorageAccountName, this.AzureStorageKey));
         }
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
